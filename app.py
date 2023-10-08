652db70b98e8f1bd7294f72c37e941407dec04e6
JawaharSaiNathani <jawaharsainathani@gmail.com>
2023-10-08 17:50:31 +0530

    Hosting on PythonAnywhere

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
-import os
 from flask import Flask, render_template, request
-import urllib
 from flask import jsonify
-from pymongo import MongoClient
-from bson.objectid import ObjectId
-
+import json
+import requests
 
 app = Flask(__name__)
 
-username = urllib.parse.quote_plus('gamemaster')
-password = urllib.parse.quote_plus('Kuhu2@way')
-client = MongoClient("mongodb://%s:%s@cluster0-shard-00-00.s6zrk.mongodb.net:27017,cluster0-shard-00-01.s6zrk.mongodb.net:27017,cluster0-shard-00-02.s6zrk.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-hzo655-shard-0&authSource=admin&retryWrites=true&w=majority" % (username, password))
-db = client.get_database('profile_db')
+data_file = open('data.json')
+data = json.load(data_file)
 
-footer_message = list(db.footer_message.find({}))[0]['_message']
-copyright_message = list(db.copyright_message.find({}))[0]['_message']
+footer_message = data['footer_message'][0]['_message']
+copyright_message = data['copyright_message'][0]['_message']
 
 @app.route("/")
 def index():
-    display_tags = list(db.display_tags.find({}))
+    display_tags = data["display_tags"]
     ftag = ''
     for tag in display_tags:
         ftag += tag['_title'] + ','
@@ -27,68 +22,62 @@ def index():
 
 @app.route("/experience/<id>")
 def experience(id):
-    experiences = list(db.experiences.find({'_id':ObjectId(id)}))
-    experiences = experiences[0]
+    experiences = data['experiences']
+    experiences = [experiences[i] for i in range(len(experiences)) if experiences[i]["_id"] == id][0]
     return render_template("experience.html", experience_info=experiences, _footer_msg=footer_message, _copyright_msg=copyright_message)
 
 @app.route("/project/<id>")
 def project(id):
-    projects = list(db.all_projects.find({'_id':ObjectId(id)}))
-    projects = projects[0]
-    print(projects["_tech"])
+    projects = data['all_projects']
+    projects = [projects[i] for i in range(len(projects)) if projects[i]["_id"] == id][0]
     return render_template("project.html", project_info=projects, _footer_msg=footer_message, _copyright_msg=copyright_message)
 
 @app.route("/certificates")
 def certificates():
-    certificates = list(db.certificates.find({}))
-    for certificate in certificates:
-        certificate['_id'] = str(certificate['_id'])
+    certificates = data['certificates']
     return render_template("certificates.html", _certificates=certificates, _footer_msg=footer_message, _copyright_msg=copyright_message)
 
 @app.route("/projects")
 def projects():
-    projects = list(db.all_projects.find({}))
-    for project in projects:
-        project['_id'] = str(project['_id'])
+    projects = data['all_projects']
     return render_template("projects.html", _projects=projects, _footer_msg=footer_message, _copyright_msg=copyright_message)
 
 @app.route("/get_certificates")
 def get_certificates():
-    certificates = list(db.certificates.find({}))
-    for certificate in certificates:
-        certificate['_id'] = str(certificate['_id'])
-    return jsonify(certificate)
+    certificates = data['certificates']
+    return jsonify(certificates)
 
 @app.route("/api/get_display_messages", methods=['GET'])
 def get_display_tags():
-    display_messages = list(db.display_message.find({}))
+    display_messages = data['display_message']
     return jsonify(display_messages[0]['_message'])
 
 @app.route("/api/get_experiences", methods=['GET'])
 def get_experiences():
-    experiences = list(db.experiences.find({}))
-    for experience in experiences:
-        experience['_id'] = str(experience['_id'])
+    experiences = data['experiences']
     return jsonify(experiences)
 
 @app.route("/api/get_educationDetails", methods=['GET'])
 def get_educationDetails():
-    educations = list(db.education.find({}))
-    for education in educations:
-        education['_id'] = str(education['_id'])
+    educations = data['education']
     return jsonify(educations)
 
 @app.route("/api/get_mainProjects", methods=['GET'])
 def get_mainProjects():
-    projects = list(db.main_projects.find({}))
-    for project in projects:
-        project['_id'] = str(project['_id'])
+    projects = data['main_projects']
     return jsonify(projects)
 
 @app.route("/api/messsage_recieved", methods=['POST'])
 def get_msg():
-    db.messages.insert_one({'_name': request.form.get('_name'), '_email': request.form.get('_email'), '_subject': request.form.get('_subject'), '_comment': request.form.get('_comment')})
+    message = "name - " + request.form.get('_name') + '\nemail - ' + request.form.get('_email') + '\n\n' + request.form.get('_comment')
+    requests.post(
+		"https://api.mailgun.net/v3/sandboxdc6e622c808a495d8771998950d967fd.mailgun.org/messages",
+		auth=("api", "b051da82ef10eaaacf1a2568eec015c4-77316142-8248c3d0"),
+		data={"from": "mailgun@sandboxdc6e622c808a495d8771998950d967fd.mailgun.org",
+			"to": ["jawaharsainathani@gmail.com"],
+			"subject": request.form.get('_subject'),
+			"text": message})
     return jsonify({'message': "Message Recieved"})
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False)
